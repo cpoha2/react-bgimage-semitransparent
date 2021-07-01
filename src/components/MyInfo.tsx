59bde5880be31d25df3693478b91d1bcf52f0b86
cpoha2 <cpoha2@gmail.com>
2021-07-01 18:28:18 -0400

    Box and span two size fonts on same line.

--- src/components/MyInfo.tsx
+++ src/components/MyInfo.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import Paper from '@material-ui/core/Paper';
-import Button from '@material-ui/core/Button';
+import {Paper, Button, Box} from '@material-ui/core';
 import Image from '../assets/myinfo_bg5.png';
 
 import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
@@ -36,7 +35,15 @@ const useStyles = makeStyles((theme: Theme) =>
     buttonSharpEdge: {
       borderRadius: '0',
       border: '3px solid currentColor',
-    }
+    },
+    fontLarge: {
+      fontFamily: 'Arial, sans-serif',
+      fontSize: '1.5rem',
+    },
+    fontMedium: {
+      fontFamily: 'Arial, sans-serif',
+      fontSize: '1.25rem',
+    },
   }),
 );
 
@@ -64,13 +71,18 @@ const MyInfo=(props: any) => {
            <Paper square className={classes.paperTop2}>
                <h2>Different rgba value for top paper</h2>
                <p>Content goes here.</p>
-               <br/>
+               
                <Button variant="outlined" className={classes.buttonSharpEdge}>ADD</Button>
-               <br/>
+               
                <p>This is more content to see the image.</p>
+               <span className={classes.fontLarge}>My Information Goes Here&nbsp;</span>
+               <span className={classes.fontMedium}>(This is where my information goes)</span>
                <br/>
+               <Box component="span" className={classes.fontLarge}>This is large font.&nbsp;</Box>
+               <Box component="span" className={classes.fontMedium}>(This is smaller font.)</Box>
            </Paper>
         </Paper>
+        
     </div>;
 }
 
