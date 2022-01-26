3221ff644df41485e1758312f837d0f0cea13321
cpoha2 <cpoha2@gmail.com>
2022-01-25 21:26:05 -0500

    svg icons that do not work yet.

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import MyInfo from './components/MyInfo';
 import SearchStuff from './components/SearchStuff';
 import { AppStore } from './store/AppStore';
 import MyAccordionTest from './components/accordions/MyAccordionTest';
+import SvgIconTest from './components/SvgIconTest';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -34,6 +35,9 @@ function App() {
         <Grid container spacing={0}>
           <Grid item xs={12}>
             <Paper className={classes.paperHeaderFooter}>HEADER</Paper>
+          </Grid>
+          <Grid item xs={12}>
+            <SvgIconTest/>
           </Grid>
           <Grid item xs={12}>
             <SearchStuff />
